c01685b323c7792871f1f7d0291ee4a262f61994
原秋柏 <1020693752@qq.com>
2025-04-24 02:00:38 +0800

    添加了新的ActorStats类
    添加 ActorStats 类及其相关功能
    
    在 `ActorStats.cs` 文件中新增 `ActorStats` 类，继承自 `MonoBehaviour`。该类包含角色的多个属性，如 `UniqueID`、`ActorID`、`ActorName`、`Hp`、`MaxHp`、`attack` 和 `moveSpeed`。引入 `BuffSystem.BuffContainer` 管理 Buff 列表，并实现 `Awake` 和 `Update` 方法以初始化和更新 Buff。增加了调试方法 `Test_PrintObjectStateInfo`，用于输出角色状态信息。

---
+++ ActorStats.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+
+// 对象属性
+namespace ActorStatsSystem
+{
+	/// <summary>
+	/// 对象属性基类
+	/// Author: Firstname Lastname
+	/// </summary>
+	public class ActorStats : MonoBehaviour
+	{
+		public string UniqueID;                         // 唯一ID
+		public string ActorID;                          // 对象ID
+		public string ActorName;                        // 对象名称
+
+		public float Hp = 100f;                         // 当前血量
+		public float MaxHp = 100f;                      // 最大血量
+		public float OriginalMaxHp = 100f;              // 初始最大血量
+
+		public float attack = 10;                       // 伤害
+		public float OriginalAttack = 10;               // 初始伤害
+		public float defence = 10;                      // 防御
+
+		public float moveSpeed = 10;                    // 移动速度
+
+		public bool IsDead = false;                     // 是否死亡
+		public bool IsInvulnerable = false;             // 是否无敌
+
+		public AttributeMultipliers stateCoefficient;       // 状态系数
+		public ActorsContainer SceneObjectList;         // 场景对象表
+		public ActorStatsList SceneObjectStateList;     // 场景对象属性表
+
+		BuffSystem.BuffContainer BuffList =
+			new BuffSystem.BuffContainer();                  // Buff列表
+
+		void Awake()
+		{
+			BuffList.InitBuffContainer(this);                // 初始化Buff表
+
+			// 随机生成唯一ID
+			for (int i = 0; i < 8; i++)
+			{
+				UniqueID += Random.Range(0, 10).ToString();
+			}
+			SceneObjectList.AddActor(UniqueID, gameObject);        // 注册对象到场景对象表
+		}
+
+		void Update()
+		{
+			if (IsDead)
+			{
+				return;
+			}
+
+			// 更新Buff时间
+			BuffList.UpdateBuffTimeLife();
+
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				Test_PrintObjectStateInfo();
+			}
+		}
+
+		/// <summary>
+		/// Buff添加接口
+		/// </summary>
+		/// <param name="newBuff"></param>
+		public void AddBuff(BuffSystem.Buff newBuff)
+		{
+			BuffList.AddBuff(newBuff);
+		}
+
+
+
+		#region DebugTest
+
+		public void Test_PrintObjectStateInfo()
+		{
+			Debug.Log("ObjectUID: " + UniqueID +
+				" ObjectName: " + ActorName + "MoveSpeed: " + moveSpeed);
+		}
+
+		#endregion
+	}
+}
